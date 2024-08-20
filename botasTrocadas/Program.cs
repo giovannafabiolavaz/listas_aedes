f7faf955748a67a07450f3c18aa53eb5af566bdf
Giovanna Fabiola Vaz <giovannafabiolavaz@gmail.com>
2024-08-19 21:20:48 -0300

    Exercicios do verde (Botas trocadas, Supermercado e Plano Internet

---
+++ botasTrocadas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int[] Direita = new int[31]; //tamanhos das botas vão de 30 a 60, logo são 31 algarismos
+        int[] Esquerda = new int[31];
+
+        for (int i = 0; i < N; i++)
+        {
+            var linha = Console.ReadLine().Split(); //separa os dados "tamanho" e "lado" quando houver um espaço " ".
+            //utiliza-se var pois este tipo se "adequa" aos outros tipos
+            int tamanho = int.Parse(linha[0]) - 30; 
+            char lado = linha[1][0];
+
+            if (lado == 'D')
+            {
+                Direita[tamanho]++;
+            }
+            else if (lado == 'E')
+            {
+                Esquerda[tamanho]++;
+            }
+        }
+        int pares = 0;
+        for (int i = 0; i < 31; i++)
+        {
+            pares += Math.Min(Direita[i], Esquerda[i]);
+        }
+        Console.WriteLine(pares);
+    }
+}
